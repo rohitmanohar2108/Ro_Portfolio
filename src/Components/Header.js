1a772dd8b6b8c660e45b9e951c562d9ff0d61dc8
Rohit Manohar <manoharrohit372@gmail.com>
2024-08-21 18:28:32 +0530

    done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,12 +6,32 @@ const Header = () => {
   return (
     <header className="bg-white text-black p-4 fixed w-full mt-6">
       <nav className="container mx-auto flex justify-between">
-        <div className="text-xl font-bold">My Portfolio</div>
-        <div className="space-x-4">
-          <Link to="/" className="hover:text-gray-300">Home</Link>
-          <Link to="/experience" className="hover:text-gray-300">Experience</Link>
-          <Link to="/projects" className="hover:text-gray-300">Projects</Link>
-          <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+        <div className="text-xl mt-3 font-bold">My Portfolio</div>
+        <div className="space-x-4 flex">
+          <Link to="/" className="relative group">
+            <div className="px-8 py-4 relative z-10 group-hover:text-white">
+              Home
+            </div>
+            <span className="absolute inset-0 bg-pink-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 "></span>
+          </Link>
+          <Link to="/experience" className="relative group">
+            <div className="px-8 py-4 relative z-10 group-hover:text-white">
+              Experience
+            </div>
+            <span className="absolute inset-0 bg-pink-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
+          </Link>
+          <Link to="/projects" className="relative group">
+            <div className="px-8 py-4 relative z-10 group-hover:text-white">
+              Projects
+            </div>
+            <span className="absolute inset-0 bg-pink-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
+          </Link>
+          <Link to="/contact" className="relative group">
+            <div className="px-8 py-4 relative z-10 group-hover:text-white">
+              Contact
+            </div>
+            <span className="absolute inset-0 bg-pink-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 "></span>
+          </Link>
         </div>
       </nav>
     </header>
