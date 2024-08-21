2a62bbd308812258017918697714879d150ed45f
Rohit Manohar <manoharrohit372@gmail.com>
2024-08-21 21:57:15 +0530

    done

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,10 +1,91 @@
-// src/pages/Experience.js
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationDataTwo from "../Animations/Animationtwo.json"
+
 
 const Experience = () => {
+
+  const defaultOptionsTwo = {
+    loop: true,
+    autoplay: true,
+    animationData: animationDataTwo, // Use the first animation data
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold">Experience</h1>
+    <div className="min-h-screen bg-black">
+      <div className="flex justify-center items-start space-x-8">
+        {/* Left Side: Experience Section */}
+        <div className="max-w-2xl mt-20">
+          <div className="mb-4">
+            <div className="border rounded-lg border-cyan-400 p-4 shadow-lg shadow-indigo-500/50">
+              <h2 className="text-3xl font-courier-prime text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500 experience-heading">
+                Experience
+              </h2>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="flex items-start mb-12 timeline-item">
+              <div className="w-full pr-8">
+                <div className="relative border-l border-cyan-400 pl-6">
+                  <div className="absolute w-4 h-4 bg-cyan-400 rounded-full -left-2 top-1/2 transform -translate-y-1/2"></div>
+                  <div className="mb-8">
+                    <h3 className="text-2xl font-courier-prime font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
+                      Member, TEDx NITK Surathkal Media Team
+                    </h3>
+                    <span className="block mb-2 text-sm font-courier-prime leading-none text-gray-400">
+                      Dec 2022 - May 2026
+                    </span>
+                    <p className="mb-2 text-base font-courier-prime text-white dark:text-gray-300">
+                      • Designed posters and promotional materials for TEDx
+                      events, enhancing visual communication and branding
+                      efforts.
+                    </p>
+                    <p className="mb-2 text-base font-courier-prime text-white dark:text-gray-300">
+                      • Collaborated on media campaigns to boost event
+                      visibility and engagement.
+                    </p>
+                    <p className="mb-2 text-base font-courier-prime text-white dark:text-gray-300">
+                      • Technologies Used: Canva, Illustrator
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex-shrink-0 w-1 border-l border-gray-300 h-full"></div>
+            </div>
+            <div className="flex items-start timeline-item">
+              <div className="w-full pr-8">
+                <div className="relative border-l border-cyan-400 pl-6">
+                  <div className="absolute w-4 h-4 bg-cyan-400 rounded-full -left-2 top-1/2 transform -translate-y-1/2"></div>
+                  <div className="mb-8">
+                    <h3 className="text-2xl font-courier-prime font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
+                      Student, National Institute of Technology Karnataka
+                    </h3>
+                    <span className="block mb-2 text-sm font-courier-prime leading-none text-gray-400">
+                      Aug 2021 - Present
+                    </span>
+                    <p className="mb-2 text-base font-courier-prime text-white dark:text-gray-300">
+                      • Pursuing a Bachelor’s degree in Computer Science.
+                    </p>
+                    <p className="mb-2 text-base font-courier-prime text-white dark:text-gray-300">
+                      • Engaged in various projects and coursework related to
+                      computer science and software development.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex-shrink-0 w-1 border-l border-gray-300 h-full"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side: Lottie Animation */}
+        <div className="ml-8">
+          <Lottie options={defaultOptionsTwo} height={500} width={500} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,15 @@
-import React, { useState, useRef } from 'react';
-import animationData from '../Animations/Animation.json';
+import React, { useState, useRef } from "react";
+import animationData from "../Animations/Animation.json";
 import { motion } from "framer-motion";
-import Lottie from 'lottie-react';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'; // Import React Icons
+import Lottie from "lottie-react";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa"; // Import React Icons
 
 const quotes = [
   {
@@ -13,17 +20,20 @@ const quotes = [
   {
     text: " 'Success is a lousy teacher. It seduces smart people into thinking they can't lose.'",
     author: "Bill Gates",
-    image: "https://pbs.twimg.com/profile_images/1674815862879178752/nTGMV1Eo_400x400.jpg",
+    image:
+      "https://pbs.twimg.com/profile_images/1674815862879178752/nTGMV1Eo_400x400.jpg",
   },
   {
     text: "' I think it is possible for ordinary people to choose to be extraordinary.'",
     author: "Elon Musk",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThCPblxoSQ8E0dWaakrQDUt73M_hS63hE9cg&s",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThCPblxoSQ8E0dWaakrQDUt73M_hS63hE9cg&s",
   },
   {
     text: "'Move fast and break things. Unless you are breaking stuff, you are not moving fast enough.'",
     author: "Mark Zuckerberg",
-    image: "https://media.vanityfair.com/photos/5cf71b7e5d0130aa612df063/master/pass/zuckerberg1.jpg",
+    image:
+      "https://media.vanityfair.com/photos/5cf71b7e5d0130aa612df063/master/pass/zuckerberg1.jpg",
   },
   {
     text: "'See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too.'",
@@ -33,12 +43,14 @@ const quotes = [
   {
     text: "'If you're changing the world, you're working on important things. You're excited to get up in the morning.'",
     author: "Larry Page",
-    image: "https://imageio.forbes.com/specials-images/imageserve/5c76bcaaa7ea43100043c836/0x0.jpg?format=jpg&crop=1795,1795,x227,y22,safe&height=416&width=416&fit=bounds",
+    image:
+      "https://imageio.forbes.com/specials-images/imageserve/5c76bcaaa7ea43100043c836/0x0.jpg?format=jpg&crop=1795,1795,x227,y22,safe&height=416&width=416&fit=bounds",
   },
   {
     text: " 'You don’t need to be a genius or a visionary, or even a college graduate for that matter, to be successful. You just need framework and a dream.'",
     author: "Michael Dell",
-    image: "https://imageio.forbes.com/specials-images/imageserve/615c93f9da61f2ff5b9ecf9b/0x0.jpg?format=jpg&crop=1678,1679,x0,y118,safe&height=416&width=416&fit=bounds",
+    image:
+      "https://imageio.forbes.com/specials-images/imageserve/615c93f9da61f2ff5b9ecf9b/0x0.jpg?format=jpg&crop=1678,1679,x0,y118,safe&height=416&width=416&fit=bounds",
   },
 ];
 
@@ -59,7 +71,16 @@ const Home = () => {
   };
 
   // Array of words to animate
-  const words = ["I", "am", "Rohit,", "Computer", "Science", "Undergrad", "at", "NITK"];
+  const words = [
+    "I",
+    "am",
+    "Rohit,",
+    "Computer",
+    "Science",
+    "Undergrad",
+    "at",
+    "NITK",
+  ];
 
   return (
     <div className="min-h-screen bg-black flex flex-col px-8">
@@ -121,25 +142,25 @@ const Home = () => {
 
         {/* Right Side: Lottie Animation */}
         <div className="mr-16">
-          <Lottie 
-            animationData={animationData} 
-            loop={true} 
-            autoplay={true} 
-            style={{ height: 600, width: 600 }} 
+          <Lottie
+            animationData={animationData}
+            loop={true}
+            autoplay={true}
+            style={{ height: 600, width: 600 }}
           />
         </div>
       </div>
 
       {/* Bottom: Quotes Section */}
-      <div className="mt-16  bg-teal-400  flex items-center">
+      <div className="mt-16 bg-teal-400 flex items-center">
         <div className="w-full mx-auto relative">
           <div className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-4 ml-5 rounded-full cursor-pointer hover:bg-gray-800 z-10">
             <FaArrowLeft onClick={handlePrevQuote} />
           </div>
-          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-4 mr-5  rounded-full cursor-pointer hover:bg-gray-800 z-10">
+          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-4 mr-5 rounded-full cursor-pointer hover:bg-gray-800 z-10">
             <FaArrowRight onClick={handleNextQuote} />
           </div>
-          <div className="w-full px-7 py-8 bg-black bg-opacity-30  shadow-lg shadow-cyan-500/50 border-multicolor outline hover:outline-2 ring-2 relative">
+          <div className="w-full px-7 py-8 bg-black bg-opacity-30 shadow-lg shadow-cyan-500/50 border-multicolor outline hover:outline-2 ring-2 relative">
             <div className="flex flex-col items-center space-y-4">
               <img
                 src={quotes[currentQuoteIndex].image}
@@ -155,6 +176,77 @@ const Home = () => {
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* About Me Section */}
+
+      <div className="mt-16 p-8 bg-black bg-opacity-30 rounded-lg shadow-lg flex items-center">
+        <img
+          src="https://media-mxp1-1.cdn.whatsapp.net/v/t61.24694-24/453603651_520834290411384_6429731950094255849_n.jpg?ccb=11-4&oh=01_Q5AaIC-9v5GugtOFEMMBYvFX3frZuTI9ad7p9dp1jxMl6CEx&oe=66CDCAFF&_nc_sid=5e03e0&_nc_cat=102"
+          alt="Rohit"
+          className="w-56 h-56 rounded-full mr-8"
+        />
+        <div>
+          <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-teal-500 px-4 py-2 text-3xl font-courier-prime mb-3  text-teal-400">
+            About Me
+          </h3>
+          <p className="text-white text-xl font-courier-prime">
+            I am a Computer Science Student at NITK. I have a deep passion for
+            coding and constantly strive to enhance my skills by learning new
+            technologies. I love tackling challenging problems and finding
+            innovative solutions. My areas of interest include web development,
+            programming, and many more. I am always eager to collaborate on
+            exciting projects and learn from others in the field. When I am not
+            coding, I enjoy exploring the latest tech trends and participating
+            in hackathons and coding competitions. Let's connect and create
+            something amazing together! Whether it's building a new app, diving
+            into a complex algorithm, or simply exchanging knowledge, I am
+            excited to engage with like-minded individuals and contribute to the
+            tech community.
+          </p>
+          <div className="flex mt-4 space-x-4 z-20 relative">
+            <a
+              href="https://github.com/rohitmanohar2108"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaGithub className="text-3xl hover:text-teal-400 transition-colors" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rohit-manohar-80b949207/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaLinkedin className="text-3xl hover:text-teal-400 transition-colors" />
+            </a>
+            <a
+              href="https://www.instagram.com/rohitmanohar2108/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaInstagram className="text-3xl hover:text-teal-400 transition-colors" />
+            </a>
+            <a
+              href="https://twitter.com/imrohit372"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaTwitter className="text-3xl hover:text-teal-400 transition-colors" />
+            </a>
+            <a
+              href="https://drive.google.com/file/d/1Rdddm_h-MFgcIkppo7uDtpQ-V-4rFZN2/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shadow-md shadow-white  text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-500 font-normal font-courier-prime text-xl py-2 px-4 rounded-lg hover:bg-violet-700 transition-colors transition-transform transform hover:scale-105"
+            >
+              View Resume
+            </a>
           </div>
         </div>
       </div>
